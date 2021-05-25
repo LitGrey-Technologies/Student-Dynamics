a819923e8927e88cc738e11031c1200a0a78489f
Ittyab Rehman <ittyaboffical@gmail.com>
2021-05-25 15:31:53 +0500

    files added

---
+++ Student.Dynamics/Student.Dynamics/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace Student.Dynamics.App.Desktop
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
